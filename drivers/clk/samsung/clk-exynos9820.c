a46717bc0d0770d1ddfe19440ebeee1f446e9d1a
chiffathefox <chiffathefoxx@gmail.com>
2026-07-25 16:29:38 +0300

    clk: exynos9820: add an empty CPUCL0 block
    These clocks probably will never be used by any consumers and can be left out. The main point of this block is to match the DRCG_EN and MEMCLK downstream register values for this block

--- drivers/clk/samsung/clk-exynos9820.c
+++ drivers/clk/samsung/clk-exynos9820.c
@@ -2941,6 +2941,25 @@ static const struct samsung_cmu_info mif_cmu_info __initconst = {
 	.drcg_offset		= EXYNOS9820_DRCG_EN_OFFSET,
 };
 
+/*
+ * Register offsets for CMU_CPUCL0 (0x1d020000)
+ */
+#define CPUCL0_CMU_CPUCL0_CONTROLLER_OPTION		0x0800
+
+static const unsigned long cpucl0_clk_regs[] __initconst = {
+	CPUCL0_CMU_CPUCL0_CONTROLLER_OPTION,
+};
+
+static const struct samsung_cmu_info cpucl0_cmu_info __initconst = {
+	.clk_regs		= cpucl0_clk_regs,
+	.nr_clk_regs		= ARRAY_SIZE(cpucl0_clk_regs),
+	.auto_clock_gate	= true,
+	.gate_dbg_offset	= EXYNOS9820_GATE_DBG_OFFSET,
+	.option_offset		= CPUCL0_CMU_CPUCL0_CONTROLLER_OPTION,
+	.drcg_offset		= EXYNOS9820_DRCG_EN_OFFSET,
+	.memclk_offset		= EXYNOS9820_MEMCLK_OFFSET,
+};
+
 static int __init exynos9820_cmu_probe(struct platform_device *pdev)
 {
 	const struct samsung_cmu_info *info;
@@ -2974,6 +2993,9 @@ static const struct of_device_id exynos9820_cmu_of_match[] = {
 	}, {
 		.compatible = "samsung,exynos9820-cmu-mif",
 		.data = &mif_cmu_info,
+	}, {
+		.compatible = "samsung,exynos9820-cmu-cpucl0",
+		.data = &cpucl0_cmu_info,
 	}, {
 	},
 };
